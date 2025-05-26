3de056f1dd5e951045be78ade80fc13cfa104d8f
Hugo Merzisen <hugo.merzisen@meteo.fr>
2025-05-26 17:56:08 +0200

    copilot

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      window = {
+        layout = 'horizontal', -- 'vertical', 'horizontal', 'float', 'replace'
+      },
+      mappings = {
+        -- Use tab for completion
+        complete = {
+          detail = 'Use @<Tab> or /<Tab> for options.',
+          insert = '<C-Tab>',
+        },
+        -- Close the chat
+        close = {
+          normal = 'q',
+          insert = '<C-c>',
+        },
+        -- Reset the chat buffer
+        reset = {
+          normal = '<C-r>',
+          insert = '<C-r>',
+        },
+        -- Submit the prompt to Copilot
+        submit_prompt = {
+          normal = '<CR>',
+          insert = '<C-CR>',
+        },
+        -- Accept the diff
+        accept_diff = {
+          normal = '<C-y>',
+          insert = '<C-y>',
+        },
+        -- Show help
+        show_help = {
+          normal = 'g?',
+        },
+      },
+    },
+    -- See Configuration section for options
+  },
+  -- See Commands section for default commands if you want to lazy load on them
+}
