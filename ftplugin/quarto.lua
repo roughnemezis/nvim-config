fe33dc8d6fed91578fef475a2dc97fffaf25c861
Hugo Merzisen <hugo.merzisen@meteo.fr>
2025-07-10 16:51:27 +0200

    render-markdown + folding qmd config

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -4,6 +4,9 @@ vim.bo.shiftwidth = 4
 vim.opt_local.foldmethod = 'expr'
 vim.opt_local.foldexpr = "v:lua.require'custom.plugins.folding'.markdown_foldexpr()"
 vim.opt_local.foldlevel = 99
+vim.opt_local.foldtext = ''
+vim.opt_local.fillchars = 'fold: '
+vim.opt.foldcolumn = '3'
 vim.cmd 'highlight! link Folded DiffText'
 
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWrite' }, {
@@ -16,3 +19,5 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWrite' }, {
     end
   end,
 })
+
+vim.cmd 'highlight Folded guibg=NONE'
--- lua/plugins/quarto-and-markdown.lua
+++ lua/plugins/quarto-and-markdown.lua
@@ -12,32 +12,43 @@ return {
       'jmbuhr/otter.nvim',
     },
   },
-  { -- highlight markdown headings and code blocks etc.
-    'lukas-reineke/headlines.nvim',
-    enabled = true,
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    config = function()
-      require('headlines').setup {
-        quarto = {
-          query = vim.treesitter.query.parse(
-            'markdown',
-            [[
-                (fenced_code_block) @codeblock
-                ]]
-          ),
-          codeblock_highlight = 'CodeBlock',
-          treesitter_language = 'markdown',
-        },
-        markdown = {
-          query = vim.treesitter.query.parse(
-            'markdown',
-            [[
-                (fenced_code_block) @codeblock
-                ]]
-          ),
-          codeblock_highlight = 'CodeBlock',
-        },
-      }
-    end,
+  -- { -- highlight markdown headings and code blocks etc.
+  --   'lukas-reineke/headlines.nvim',
+  --   enabled = true,
+  --   dependencies = 'nvim-treesitter/nvim-treesitter',
+  --   config = function()
+  --     require('headlines').setup {
+  --       quarto = {
+  --         query = vim.treesitter.query.parse(
+  --           'markdown',
+  --           [[
+  --               (fenced_code_block) @codeblock
+  --               ]]
+  --         ),
+  --         codeblock_highlight = 'CodeBlock',
+  --         treesitter_language = 'markdown',
+  --       },
+  --       markdown = {
+  --         query = vim.treesitter.query.parse(
+  --           'markdown',
+  --           [[
+  --               (fenced_code_block) @codeblock
+  --               ]]
+  --         ),
+  --         codeblock_highlight = 'CodeBlock',
+  --       },
+  --     }
+  --   end,
+  -- },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    ft = { 'markdown', 'quarto' },
+    heading = { border = true },
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
 }
