14efd2b038c81b0ccda7c36d9994af00c042cd05
Hugo Merzisen <hugo.merzisen@meteo.fr>
2025-07-10 16:48:33 +0200

    refresh folding on save for quarto

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -6,11 +6,13 @@ vim.opt_local.foldexpr = "v:lua.require'custom.plugins.folding'.markdown_foldexp
 vim.opt_local.foldlevel = 99
 vim.cmd 'highlight! link Folded DiffText'
 
-vim.api.nvim_create_autocmd('BufEnter', {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWrite' }, {
   pattern = '*',
   callback = function()
     if vim.bo.filetype == 'quarto' then
       require('custom.plugins.folding').compute_fold_levels()
+      local keys = vim.api.nvim_replace_termcodes('<esc>zx', true, false, true)
+      vim.api.nvim_feedkeys(keys, 'm', false)
     end
   end,
 })
