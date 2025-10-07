38a2f73edbcf7a8c81a713ce3da0864bdbe2842e
Hugo Merzisen <hugo.merzisen@meteo.fr>
2025-10-07 11:06:37 +0200

    delete buffer in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -55,11 +55,17 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            n = {
+              ['<c-d>'] = require('telescope.actions').delete_buffer,
+            },
+            i = {
+              ['<C-h>'] = 'which_key',
+              ['<c-d>'] = require('telescope.actions').delete_buffer,
+            },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
