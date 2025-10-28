228b78b1f10915ef5f59bef60670db1a8f5894bb
Hugo Merzisen <hugo.merzisen@meteo.fr>
2025-10-28 09:04:29 +0100

    additionnal config after plugins install

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require 'config.global'
 require 'config.commands'
 require 'config.keymaps'
 require 'config.lazy'
+require 'config.post_plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
